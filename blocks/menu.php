94611134967ef1085f200f176cc0bfe63e3fb70d
Sergeizd <79394461+Sergeizd@users.noreply.github.com>
2021-02-21 09:47:38 +0300

    Add files via upload

---
+++ blocks/menu.php
@@ -0,0 +1,35 @@
+			
+			<div id="">
+				<ul id="" class="">
+					<li class="logo"><a href="main"><img src="img/logo.png" alt="MZ image" id="logo" ></a></li>
+					<li><a href="main">Главная</a></li>
+					<li><a href="about">Обо мне</a></li>
+					<li><a href="gallery-difrent.php">Портфолио</a>
+						<ul>
+							<li><a href="gallery-difrent.php">Основной</a></li>
+							<li><a href="gallery-wedding.php">Свадебные</a></li>
+							<li><a href="gallery-child.php">Дети</a></li>
+							<li><a href="gallery-pregnant.php">В ожидании чуда</a></li>
+							<li><a href="gallery-catalog.php">Каталог</a></li>
+							<li><a href="gallery-nude.php">Арт-Ню (18+)</a></li>
+							<li><a href="gallery-landscape.php">Пейзаж</a></li>
+							<li><a href="before-after">До и После</a></li>
+						</ul>
+					</li>
+					<li><a href="blog">Блог</a></li>
+					<li><a href="Novosti">Новости</a></li>
+					<li><a href="comments.php">Отзывы</a></li>
+					<li><a href="price">Цены</a>
+						<!--Img Sale-->
+						<div id="fixedsale">
+							<a href="price"> <img src="img/sale.png" alt='Акция'></a>
+						</div>
+						<!-- ends img sale-->
+					</li>
+					<li><a href="partners">Сотрудничество</a></li>
+					<li><a href="articles">Все о фото</a></li>
+					<li><a href="contact">Контакты</a></li>
+					<li class="logo-zagl"></li>
+				</ul>
+			</div>
+			
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+     <head>
+          <meta charset="utf-8">
+          <link rel="stylesheet" type="text/css" href="style.css" />
+          <title>Honda NC FAQ</title>
+     </head>
+     <body>
+	   <div class="wrapper">
+	     <div class="head">
+		    <h1>Honda NC FAQ</h1>
+		 </div>
+		 <div class="sidebar">
+		    <h2>Меню</h2>
+				<?php
+					require_once ("blocks/menu.php");
+				?>
+		 </div>
+		 <div class="content">
+            <h2>Контент</h2>		 
+		 </div>	
+		 <div class="advert">
+            <h2>Рекламный блок</h2>		 
+		 </div>	
+		 <div class="footer">
+            <h2>Подвал</h2>		 
+		 </div>
+	   </div>
+	 </body>
+</html>
